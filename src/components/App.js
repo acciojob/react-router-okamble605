78765d8b59700f926d7e90ed0527131cd4542ebe
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-27 14:33:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-import React from "react";
-import './../styles/App.css';
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <p>Welcome to the home page.</p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>This is a sample React Router program.</p>
+  </div>
+);
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
